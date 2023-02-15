14e4722cf8b55fafa9af6a4adeb0d015cff1e3b0
Joaquin Angeles <jqn.ngls@gmail.com>
2023-02-15 13:41:06 -0700

    React init

---
+++ src/components/styles/MaincontentStyle.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const MaincontentStyle = styled.section`
+    width: 85vw;
+
+    .div-title {
+        display: flex;
+        justify-content: center;
+        
+    }
+`
